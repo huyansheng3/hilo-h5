b4d37e45856a8a4ff72675e95f129c9200d41cbb
jiawenze <jiawenze@ruohua.club>
2019-04-02 08:39:46 +0800

    add wechat auth

--- dist/index.php
+++ dist/index.php
@@ -1,4 +1,12 @@
 <?php
 require('auth.php');
+?>
+<script>
+  window.$userinfo = {};
+  window.$userinfo.openid  = "<?php echo $userinfo['openid'];?>";
+  window.$userinfo.nickname  = "<?php echo $userinfo['nickname'];?>";
+  window.$userinfo.headimgurl  = "<?php echo $userinfo['headimgurl'];?>";
+</script>
+<?php
 require('index.html');
 ?>
